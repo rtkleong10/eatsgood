84db8ac2a0220abf1b9ad3871856d3c91f742dc2
rtkleong10 <rtkleong10@gmail.com>
2021-06-27 01:31:46 +0800

    [update] Renamed codefiesta to eatsgood

--- eatsgood/urls.py
+++ eatsgood/urls.py
@@ -1,4 +1,4 @@
-"""codefiesta URL Configuration
+"""eatsgood URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- eatsgood/wsgi.py
+++ eatsgood/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for codefiesta project.
+WSGI config for eatsgood project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codefiesta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eatsgood.settings')
 
 application = get_wsgi_application()
