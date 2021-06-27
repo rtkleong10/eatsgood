a899019efe308605a3680f7cdb9843cc124ad564
rtkleong10 <rtkleong10@gmail.com>
2021-06-27 16:45:28 +0800

    [update] Added favicon

--- eatsgood/urls.py
+++ eatsgood/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('workshops.urls')),
     path('', include('registration.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico'))),
     path('admin/', admin.site.urls),
 ]
 
